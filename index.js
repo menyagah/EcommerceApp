21fe3cef5acb61a10b428ea0a088e99013085093
menyagah <menyagah27@gmail.com>
2021-11-05 17:34:33 +0300

    Remove test response, add html form and input parser function

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res)=> {
-    res.send("Hey there!")
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="paswordconfirmation" placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+});
+
+const bodyParser = (req, res) => {
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');
+        const formData = {}
+        for(let pair of parsed){
+            const [key, value] = pair.split('=');
+            formData[key] = value
+        }
+        console.log(formData);
+    });
+}
+
+app.post('/', (req, res)=> {
+    
+    res.send('Account Created');
 });
 
 app.listen(3001, ()=> {
     console.log('listening');
-})
+});
