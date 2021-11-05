1b849428d645eed8497fb5bf89174ad280d0fdd8
menyagah <menyagah27@gmail.com>
2021-11-05 17:50:16 +0300

    Add manually written data parser

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ app.get('/',(req, res)=> {
     `);
 });
 
-const bodyParser = (req, res) => {
-    req.on('data', data => {
+const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') {
+       req.on('data', data => {
         const parsed = data.toString('utf8').split('&');
-        const formData = {}
+        const formData = {};
         for(let pair of parsed){
             const [key, value] = pair.split('=');
-            formData[key] = value
+            formData[key] = value;
         }
-        console.log(formData);
-    });
-}
-
-app.post('/', (req, res)=> {
+        req.body = formData;
+        next();
+    }); 
+    } else {
+        next();
+    }
     
+};
+
+app.post('/', bodyParser, (req, res)=> {
+    console.log(req.body);
     res.send('Account Created');
 });
 
